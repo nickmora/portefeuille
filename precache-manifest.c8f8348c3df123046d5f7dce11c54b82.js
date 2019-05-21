8860d6fa44a1f4316b4731ec2e5b1d283fde47dc
Nick Morales <bick.norales@gmail.com>
2019-05-21 11:32:56 -0700

    Updates

---
+++ precache-manifest.c8f8348c3df123046d5f7dce11c54b82.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "5333e6ec4a338e6e4041",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/css/main.7e75085d.chunk.css"
+  },
+  {
+    "revision": "5333e6ec4a338e6e4041",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/js/main.5333e6ec.chunk.js"
+  },
+  {
+    "revision": "c4d90e1259bcc7f00cf2",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/js/1.c4d90e12.chunk.js"
+  },
+  {
+    "revision": "f96c053333ddc21aeea0",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/js/runtime~main.f96c0533.js"
+  },
+  {
+    "revision": "d20531d71c1872ef4f62a208789fcb24",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/monogram.d20531d7.svg"
+  },
+  {
+    "revision": "67cb4bace1ef2926e94739974dbdb5ea",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/bootstrap.67cb4bac.svg"
+  },
+  {
+    "revision": "0a9363e087440c2703e9ab2359a64009",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/css3.0a9363e0.svg"
+  },
+  {
+    "revision": "9240c2722f41233cac582c4e51eff7a6",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/express.9240c272.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "cd849ab9d8ba06af809a4d72ac60f5a6",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/materialUI.cd849ab9.svg"
+  },
+  {
+    "revision": "e20af11e8aa6253ed368918ecb8fe638",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/react.e20af11e.svg"
+  },
+  {
+    "revision": "bcee418f09f79afaa45a4f40dc575e87",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/redux.bcee418f.svg"
+  },
+  {
+    "revision": "80e39c1ef88383e34df19a730d680c12",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/mongodb.80e39c1e.svg"
+  },
+  {
+    "revision": "a745dcd5c47f452d02438d466584f9a8",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/node.a745dcd5.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "https//:www.nickmora.github.io/portefeuille/static/media/html5.676e8188.svg"
+  },
+  {
+    "revision": "4d8e288fda0efb1f342c68da4db29c9f",
+    "url": "https//:www.nickmora.github.io/portefeuille/index.html"
+  }
+];
